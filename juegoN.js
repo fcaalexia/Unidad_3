81836d6d439551e9d4c677ca704b9a6382e5af86
fcaalexia <89996255+fcaalexia@users.noreply.github.com>
2021-11-24 23:07:31 -0600

    Add files via upload

---
+++ juegoN.js
@@ -0,0 +1,46 @@
+    var nombre = prompt("Jugador 1 ingrese su nombre");
+    console.log(nombre);
+    var b1 = prompt("En que posicion desea ubicar su primer barco");
+    console.log("posicion 1: " + b1);
+    var b2 = prompt("En que posicion desea ubicar su segundo barco");
+    console.log("posicion 2: " + b2);
+    var b3 = prompt("En que posicion desea ubicar su tercer barco");
+    console.log("posicion 3: " + b3);
+
+    var nombre1 = prompt("Jugador 2 ingrese su nombre");
+    console.log(nombre1);
+    var b4 = prompt("En que posicion desea ubicar su primer barco");
+    console.log("posicion 1: " + b4);
+    var b5 = prompt("En que posicion desea ubicar su segundo barco");
+    console.log("posicion 2; " + b5);
+    var b6 = prompt("En que posicion desea ubicar su tercer barco");
+    console.log("posicion 3: " + b6);
+
+    function jugador1(b1,b2,b3) {
+        var posicion1 = [];
+        posicion1.push("x");
+        console.log(posicion1);
+
+        var posicion2 = [];
+        posicion2.push("x");
+        console.log(posicion2);
+
+        var posicion3 = [];
+        posicion3.push("x");
+        console.log(posicion3);
+    }
+
+    function jugador2(b4,b5,b6) {
+        var posicion01 = [];
+        posicion01.push("x");
+        console.log(posicion01);
+
+        var posicion02 = [];
+        posicion02.push("x");
+        console.log(posicion02);
+
+        var posicion03 = [];
+        posicion03.push("x");
+        console.log(posicion03);
+    }
+
